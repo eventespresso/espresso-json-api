bb1071b144038b164cd49456f4310cf9072faa7d
Michael Nelson <michael@eventespresso.com>
2013-09-30 08:55:13 -0700

    [touch:3281]{t:20}discussed and switched plugin slug over to simple string while this version is in BETA

--- EventEspresso-API.php
+++ EventEspresso-API.php
@@ -78,11 +78,12 @@ function ee_json_api_load_pue_update() {
 		require(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php' );
 		$api_key = $org_options['site_license_key'];
 		$host_server_url = 'http://eventespresso.com';
-//		$plugin_slug = 'espresso-json-api';
-		$plugin_slug = array(
-			'premium' => array('P' => 'espresso-json-api'),
-			'prerelease' => array('B' => 'espresso-json-api-pr')
-			);
+		$plugin_slug = 'espresso-json-api-pr';
+//		TODO: uncomment when releasing LIVE
+//		$plugin_slug = array(
+//			'premium' => array('P' => 'espresso-json-api'),
+//			'prerelease' => array('B' => 'espresso-json-api-pr')
+//			);
 		$options = array(
 			'apikey' => $api_key,
 			'lang_domain' => 'event_espresso',
