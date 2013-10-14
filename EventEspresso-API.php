17076115eb7a08b81f204e411cd7cf949265e21d
Josh Feck <josh@nxnweb.com>
2013-10-14 11:07:21 -0400

    incrementing version number [touch: 3740]
    incrementing version number before uploading a new archive to the plugin library [touch: 3740]

--- EventEspresso-API.php
+++ EventEspresso-API.php
@@ -3,7 +3,7 @@
 Plugin Name: Event Espresso API Plugin
 Plugin URI: http://eventespresso.com
 Description: A JSON/XML API for Event Espresso
-Version: 2.0.3.p
+Version: 2.0.4.p
 Author: Event Espresso, (Mike Nelson)
 Requiresa at least: Event Espresso 3.1.30, and Wordpress 3.3.0
  *  For Developers: How this plugin generally works:
@@ -26,7 +26,7 @@ which corresponds to the current Event Espresso Core version.
  * puts it into JSON or XML format and prints it to the output buffer.
 */
 define('EspressoAPI_DIR_PATH',plugin_dir_path(__FILE__));
-define('EspressoAPI_VERSION','2.0.3.p');
+define('EspressoAPI_VERSION','2.0.4.p');
 
 //constants relating to responses
 define('EspressoAPI_STATUS','status');
