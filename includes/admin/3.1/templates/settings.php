45a75dff650370ee74c2685e01b14ccad5576142
Michael Nelson <michael@eventespresso.com>
2014-09-11 15:39:55 -0700

    [touch:6417]{t:20}added a little more text explaining the easy fix to ticket 6417

--- includes/admin/3.1/templates/settings.php
+++ includes/admin/3.1/templates/settings.php
@@ -30,7 +30,7 @@
 						<h3 class="hndle"><?php _e("Options",'event_espresso')?></h3>
 						<div class='inside'>
 							<div class="padding">
-								
+
 									<input type="hidden" name="<?php echo EspressoAPI_ADMIN_REAUTHENTICATE?>" id='<?php echo EspressoAPI_ADMIN_REAUTHENTICATE?>' value="false">
 									<input type="submit" class='button' id='make-api-users-reauthenticate' value="<?php _e("Force API clients to re-authenticate",'event_espresso')?>" ></input><br/>
 									<p><?php _e("By clicking the above button, all API sessions for anyone using the API (users of iphone app, iPad app, and other API clients)
@@ -38,17 +38,17 @@
 									fallen into the hands of someone who shouldn't be allowed to access your private data",'event_espresso')?></p>
 
 									<label for="<?php echo EspressoAPI_ADMIN_SESSION_TIMEOUT?>">API Session Timeout After </label>
-									<select name="<?php echo EspressoAPI_ADMIN_SESSION_TIMEOUT?>" id="<?php echo EspressoAPI_ADMIN_SESSION_TIMEOUT?>"> 
+									<select name="<?php echo EspressoAPI_ADMIN_SESSION_TIMEOUT?>" id="<?php echo EspressoAPI_ADMIN_SESSION_TIMEOUT?>">
 										<?php foreach($templateVars[EspressoAPI_ADMIN_SESSION_TIMEOUT_OPTIONS] as $optionLabel=>$optionTime){
 											$selectedHTML=$optionTime==$templateVars[EspressoAPI_ADMIN_SESSION_TIMEOUT]?'selected':'';?>
 										<option value="<?php echo $optionTime?>" <?php echo $selectedHTML?>><?php echo $optionLabel?></option>
 										<?php }?>
 									</select>
-									<p><?php _e("Force API users to re-authenticate (login) after this much time of inactivity. Requiring users to login more frequently 
+									<p><?php _e("Force API users to re-authenticate (login) after this much time of inactivity. Requiring users to login more frequently
 									may help improve security, but may also be tedious for API users.","event_espresso")?></p>
 									<br/>
 									<label for='<?php echo EspressoAPI_ALLOW_PUBLIC_API_ACCESS?>'><?php _e("Allow Public API Access?",'event_espresso');?></label>
-									
+
 									<select name="<?php echo EspressoAPI_ALLOW_PUBLIC_API_ACCESS?>" id="<?php echo EspressoAPI_ALLOW_PUBLIC_API_ACCESS?>">
 										<option value="1" <?php echo $templateVars[EspressoAPI_ALLOW_PUBLIC_API_ACCESS]?'selected':''?>><?php _e("Allow", "event_espresso");?></option>
 										<option value="0" <?php echo !$templateVars[EspressoAPI_ALLOW_PUBLIC_API_ACCESS]?'selected':''?>><?php _e("Don't Allow", "event_espresso");?></option>
@@ -56,19 +56,20 @@
 									<p><?php _e("Enabling will allow non-logged-in api clients to get certain information from your website via the API. Accessible information consists of:
 									events, event categories, dates and times of events, prices, price types, venues and questions. However, they will NOT be able to see: promocodes, attendees,
 									registrations, transactions, or answers",'event_espresso');?></p>
-									
+
 									<?php if(defined('ESPRESSO_MANAGER_PRO_VERSION')){?>
 									<label for='<?php echo EspressoAPI_SHOW_RESOURCES_I_CANT_EDIT_BY_DEFAULT?>'><?php _e("Show API Users Data They Can't Edit",'event_espresso');?></label>
-									
+
 									<select name="<?php echo EspressoAPI_SHOW_RESOURCES_I_CANT_EDIT_BY_DEFAULT?>" id="<?php echo EspressoAPI_SHOW_RESOURCES_I_CANT_EDIT_BY_DEFAULT?>">
 										<option value="1" <?php echo $templateVars[EspressoAPI_SHOW_RESOURCES_I_CANT_EDIT_BY_DEFAULT]?'selected':''?>><?php _e("Yes", "event_espresso");?></option>
 										<option value="0" <?php echo !$templateVars[EspressoAPI_SHOW_RESOURCES_I_CANT_EDIT_BY_DEFAULT]?'selected':''?>><?php _e("No", "event_espresso");?></option>
 									</select>
 									<p><?php _e("When set to 'No', API Users (eg, Event Espresso iOS users) can, by default, see only events they can edit. When set to 'Yes',
 										they will also see all 'public' events (ie, events which can be seen by anyone visiting the event list page). API Client applications can override this default.",'event_espresso');?></p>
+									<p><?php _e( 'If you are using an Event Espresso mobile app (eg the Android or iOS app) it is recommended to set this to "No", so event managers aren\'t given the impression they can edit registrations to events they don\'t manage.', 'event_espresso' );?></p>
 									<?php }?>
-									
-									
+
+
 									<label for'<?php echo EspressoAPI_DEFAULT_QUERY_LIMITS?>' id='<?php echo EspressoAPI_DEFAULT_QUERY_LIMITS?>'><?php _e("Default Query Limits",'event_espresso')?></label>
 									<p><?php _e('When an api API clients make a request to the following endpoints, and they do not specify a "limit", how many results should be returned?','event_espresso');?>
 									<p><?php _e('Note: returning a smaller number by default will increase speed, but api clients may not see all the results they would like.','event_espresso');?>
@@ -92,7 +93,7 @@
 						<div class='inside'>
 							<div class="padding">
 								<label for='<?php echo EspressoAPI_DEBUG_MODE?>'><?php _e("API Debug Mode",'event_espresso');?></label>
-									
+
 									<select name="<?php echo EspressoAPI_DEBUG_MODE?>" id="<?php echo EspressoAPI_DEBUG_MODE?>">
 										<option value="1" <?php echo $templateVars[EspressoAPI_DEBUG_MODE]?'selected':''?>><?php _e("On", "event_espresso");?></option>
 										<option value="0" <?php echo !$templateVars[EspressoAPI_DEBUG_MODE]?'selected':''?>><?php _e("Off", "event_espresso");?></option>
@@ -124,6 +125,6 @@ espresso_choose_layout($main_post_content, event_espresso_display_right_column()
 		jQuery('#make-api-users-reauthenticate').click(function(){
 			jQuery('#<?php echo EspressoAPI_ADMIN_REAUTHENTICATE?>').val('true');
 		});
-	}); 
+	});
 	//]]>
 </script>
